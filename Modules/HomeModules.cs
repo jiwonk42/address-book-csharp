1c688fa94688abb5cefd2987acf8ee68fc4d6c43
Jiwon Kang <jiwonk42@gmail.com>
2017-02-10 09:16:01 -0800

    set up a HomeModules.cs file skeleton, namespace called AddressBook

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using System.Collections.Generic;
+using AddressBook.Objects;
+
+namespace AddressBook
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      
+    }
+  }
+}
