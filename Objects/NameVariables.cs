2c765adc69c6264c64b43894ea5f3d851249ac80
Jiwon Kang <jiwonk42@gmail.com>
2017-02-10 10:44:20 -0800

    create a public class called Contact and a Contact constructors with _name, _phone, _address, _contact list, and _id variables

--- Objects/NameVariables.cs
+++ Objects/NameVariables.cs
@@ -4,6 +4,19 @@ namespace AddressBook.Objects
 {
   public class Contact
   {
-    
+    private string _name;
+    private string _phone;
+    private string _address;
+    private int _id;
+    private static List<Contact> _contact = new List<Contact> {};
+
+    public Contact(string name, string phone, string address)
+    {
+      _name = name;
+      _phone = phone;
+      _address = address;
+      _contact.Add(this);
+      _id = _contact.Count;
+    }
   }
 }
